c19392f33970dcaf72a7d25ff01fe7e8d3527ea8
ishangupta2507 <164048495+ishangupta2507@users.noreply.github.com>
2024-07-04 11:16:08 +0530

    added delete account

---
+++ users_area/delete_account.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete Account</title>
+</head>
+<body>
+    <h3 class="mb-4">Are you sure you want to delete your account?</h3>
+    <form action="" method="post" class="mt-4">
+        <div class="form-outline mb-4">
+            <input type="submit" name="delete" class="form-control w-50" value="delete account">
+        </div>
+    </form>
+</body>
+</html>
